abf14663ba8ce39b17a0a5b8e6dda40d5379ac95
lucianoreguera <regueraluciano@gmail.com>
2024-11-11 09:59:11 -0300

    Módulo 3 Sprint 2 TP 3

---
+++ Modulo3/Sprint2/tp3/src/models/SuperHero.mjs
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const superheroSchema = new mongoose.Schema(
+  {
+    nombreSuperHeroe: { type: String, required: true },
+    nombreReal: { type: String, required: true },
+    edad: { type: Number, min: 0 },
+    planetaOrigen: { type: String, default: "Desconocido" },
+    debilidad: String,
+    poderes: [String],
+    aliados: [String],
+    enemigos: [String],
+    createdAt: { type: Date, default: Date.now },
+    creador: { type: String, default: "Luciano Reguera" },
+  },
+  { collection: "Grupo-17" }
+);
+
+export default mongoose.model("SuperHero", superheroSchema);
